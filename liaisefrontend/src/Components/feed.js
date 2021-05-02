71d0b1864501278dc1d6b25ce1d0e3f7a93a87b7
kirtirajsinh <kirtiraj3738@gmail.com>
2021-05-02 20:07:22 +0530

    Added Feed Box

---
+++ liaisefrontend/src/Components/feed.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, Text, Container } from "@chakra-ui/react"
+import './feed.css';
+function Feed() {
+   return(
+    <>
+    <Container maxW="xl" centerContent>
+    <h2>Rooms</h2>
+    <Box  border="2px solid #3265B1" h="40em" w="50%" m={2}   boxShadow="dark-lg" p="6" rounded="md" bg="white">
+  Card
+</Box>
+
+    </Container>
+    
+
+ 
+    
+     </>
+   ) 
+}
+export default Feed;
